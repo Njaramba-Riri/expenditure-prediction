b670581aaff56f8ff83bd17206d5b1150a558934
Riri <jamesriri03@gmail.com>
2023-05-09 15:56:41 +0300

    update

--- flask/main.py
+++ flask/main.py
@@ -1,7 +1,22 @@
-from flask import Flask, url_for, jsonify, requests
+from flask import Flask, redirect, url_for
 
-app= Flask()
+app= Flask(__name__)
 
-@app.post("/predict")
+@app.route("/predict")
 def predict():
-    
+    return 'Hello World'
+
+@app.route('/<name>')
+def run(name):
+    return 'Name is %s!' % name
+
+@app.route('/blog/<int:PostID>')
+def new(PostID):
+    return 'Your new page is %d'% PostID
+
+@app.route('/new/<float:newno>')
+def nasty(newno):
+    return 'Your new number %.2f'% newno
+
+if __name__== '__main__':
+    app.run(debug=True)
