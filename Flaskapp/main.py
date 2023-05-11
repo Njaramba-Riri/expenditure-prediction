bdb3e3c9583441e29c339561ef09d281a15b654e
Riri <jamesriri03@gmail.com>
2023-05-11 23:49:37 +0300

    update

--- Flaskapp/main.py
+++ Flaskapp/main.py
@@ -1,22 +1,14 @@
-from flask import Flask, redirect, url_for
+import os
+from flask import Flask, redirect, url_for, request, render_template
 
-app= Flask(__name__, instance_relative_config=True)
+app= Flask(__name__, instance_relative_config=True, template_folder='templates')
 
-@app.route('/predict')
-def predict():
-    return 'Hello World'
 
-@app.route('/<name>')
-def run(name):
-    return 'Name is %s!' % name
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-@app.route('/blog/<int:PostID>')
-def new(PostID):
-    return 'Your new page is %d'% PostID
 
-@app.route('/new/<float:newno>')
-def nasty(newno):
-    return 'Your new number %.2f'% newno
 
 if __name__== '__main__':
     app.run(debug=True)
