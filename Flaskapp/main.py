db7f8a9e69c2d2a61d11a8a8f34311a4f34e5b39
Riri <jamesriri03@gmail.com>
2023-05-21 18:06:22 +0300

    update

--- Flaskapp/main.py
+++ Flaskapp/main.py
@@ -5,7 +5,6 @@ from flask import Flask, redirect, url_for, request, render_template
 
 app= Flask(__name__, instance_relative_config=True, template_folder='templates')
 
-model= with(open )
 
 app.config.from_mapping(
         SECRET_KEY='exp',
@@ -18,6 +17,32 @@ def index():
 
 @app.route('/predict', methods=['GET', 'POST'])
 def predict():
+    if request.method == 'GET':
+        return "Enter Details On The Form to Get Predictions."
+
+    if request.method == 'POST':
+        details  =  request.post.form
+        country  =  details['country']
+        age      =  details['age']
+        tmale    =  details['tmale']
+        tfemale  =  details['tfemale']
+        twith    =  details['twith']
+        purpose  =  details['purpose']
+        activity =  details['activity']
+        info     =  details['info']
+        tarr     =  details['tarr']
+        pint     =  details['pint'] 
+        pacc     =  details['pacc']
+        pfood    =  details['pfood']
+        ptra     =  details['ptra']
+        psigh    =  details['psigh']
+        pgt      =  details['pgt']
+        pins     =  details['pins']
+        nmain    =  details['nmain']
+        nzanzi   =  details['nzanzi']
+        ftrip    =  details['ftrip']  
+
+    
     return render_template('index.html')
 
 
