e93c4d44022e6537aa0c9d6f7e6c53a37e95fe75
Riri <jamesriri03@gmail.com>
2023-05-22 21:15:10 +0300

    update

--- .ipynb_checkpoints/pipelines-checkpoint.py
+++ .ipynb_checkpoints/pipelines-checkpoint.py
@@ -32,7 +32,7 @@ def preprocessor(X):
     
     text_pipeline=Pipeline([
         ("imputer", SimpleImputer(strategy='most_frequent')),
-        ("vectorizer",OrdinalEncoder()),
+        ("vectorizer",DictVectorizer(sparse=False, dtype=int)),
     ])
     
     preprocessor=ColumnTransformer([
