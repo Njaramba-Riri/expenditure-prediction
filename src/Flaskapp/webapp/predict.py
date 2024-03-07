916134e18335936a3bb35819e5824132d3f23cb1
Riri <jamesriri03@gmail.com>
2024-03-07 23:53:11 +0300

    Integrate regression model

--- src/Flaskapp/webapp/predict.py
+++ src/Flaskapp/webapp/predict.py
@@ -9,19 +9,41 @@ from sklearn.preprocessing import LabelEncoder
 from .vectorizer import vect
 
 dire = os.path.dirname(__file__)
-model = pickle.load(open(os.path.join(dire, 'admin/pickle_objects', 'Cat.pkl'), 'rb'))
+classifier = pickle.load(open(os.path.join(dire, 'mainapp/pickled', 'Cat_v3.pkl'), 'rb'))
+regressor = pickle.load(open(os.path.join(dire, 'mainapp/pickled', 'catregress.pkl'), 'rb'))
 clf = pickle.load(open(os.path.join(dire, 'admin/pickle_objects', 'classifier.pkl'), 'rb'))
 
-def predict_details(data):
+
+def staycation(stay_duration):
+    if stay_duration <= 7:
+        return "week"
+    elif stay_duration > 7 <= 14:
+        return "2 weeks"
+    elif stay_duration > 14 <= 21:
+        return "3 weeks"
+    elif stay_duration > 21 <= 28:
+        return "month"
+    elif stay_duration > 28:
+        return "eternity"
+
+def predict_category(inputs):
     categories = ['High Cost', 'Higher Cost', 'Highest Cost', 
                 'Low Cost', 'Lower Cost', 'Normal Cost']
-    le =LabelEncoder()
+    le = LabelEncoder()
     le.fit_transform(categories)
 
-    category = le.classes_[model.predict(data)[0]]
-    probability = np.max(model.predict_proba(data)).round(4) * 100 #, le.classes_)))[::-1]
+    category = classifier.predict(inputs)[0]
+    probability = np.max(classifier.predict_proba(inputs)).round(4) * 100 #, le.classes_)))[::-1]
+    # probability = classifier.predict_proba(inputs)
 
     return category, probability
+
+def predict_cost(inputs):
+    cost = regressor.predict(inputs)
+    predicted_cost = np.exp(cost)
+    
+    return predicted_cost
+
 
 def predict_feed_sentiment(feedback):
     labels = { 0: 'Negative', 1: 'Positive'}
